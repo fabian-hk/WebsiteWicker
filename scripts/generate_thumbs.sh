4feec9c604296522d261f6ef532ae4e520e1c581
Fabian Hauck <hauckfabian@gmail.com>
2021-06-08 21:51:56 +0200

    added images to gallery

--- scripts/generate_thumbs.sh
+++ scripts/generate_thumbs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-for i in `find static/img/gallery -type f ! -name "*-thumb.JPG" -name "*.JPG"`; 
+for i in `find static/img/gallery -type f ! -iname "*-thumb.JPG" -iname "*.JPG"`; 
 do 
     echo $i;
     convert $i -auto-orient -quality 60% -thumbnail 600x600 ${i%.*}-thumb.JPG;
     convert -auto-orient -strip -interlace Plane -quality 20% $i ${i%.*}.JPG;
 done
+
+rm static/img/gallery/*.jpg
